9b7ab78b69502f6777122f8937f7f1828b98b897
roy214 <roy214@github.com>
2022-07-24 15:06:59 -0500

    Multiplication

--- add.py
+++ add.py
@@ -12,5 +12,5 @@ print('The sum of {0} and {1} is {2}'.format(num1, num2, sum1))
 mul = float(num1) * float(num2)
 
 # Dispaly the multiplication
-print('The multiply of {0} and {1} is {2}'.format(num1, num2, mul))
+print('The multiplication of {0} and {1} is {2}'.format(num1, num2, mul))
 
